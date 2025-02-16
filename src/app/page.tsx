baa833ad3612de66b512a66bfd86b9a4f9a2da04
br0067149533@enel.com <jbmarreiro@ayesa.com>
2025-02-15 21:36:10 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1>Home</h1>
       <Sheet>
         <SheetTrigger>
-          <Button variant="outline">Login</Button>
+          <Button>Login</Button>
         </SheetTrigger>
         <SheetContent>
           <h2>Login</h2>
