cc438dafb30d6a48216b02c53572af80749328f4
br0067149533@enel.com <jbmarreiro@ayesa.com>
2025-02-16 13:16:09 -0300

    feat: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,14 @@ export default function Home() {
         <h2 className="text-xl font-bold">Olá, Jhon!</h2>
         <p>Sábado, 15 de fevereiro</p>
         <p>Seja bem vindo ao nosso torneio Green Life</p>
-      </div>
-      {/* Banner com a imagem do tornei */}
-      <div className="p-5">
-        <Image
-          src="/banner.jpg"
-          height={150}
-          width={700}
-          alt="Banner do torneio"
-        />
+        <div className="relative w-full h-[300px] mt-5">
+          <Image
+            src="/banner.jpg"
+            alt="Banner do torneio"
+            fill
+            className="object-cover rounded-xl"
+          />
+        </div>
       </div>
     </div>
   );
